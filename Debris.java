2bda2851343eb35c7be526100897c57bac68beb0
ChaoticProfessor <45435109+ChaoticProfessor@users.noreply.github.com>
2018-11-28 12:09:54 -0700

    Update Debris.java

--- Debris.java
+++ Debris.java
@@ -1,56 +1,50 @@
 package asteroids.participants;
 
 import static asteroids.game.Constants.*;
-import asteroids.game.Controller;
 import asteroids.game.Participant;
 import java.awt.Shape;
 import java.awt.geom.*;
 
-public abstract class Debris extends Participant
+public class Debris extends Participant
 {
-    
+
     /** The outline of the asteroid */
     private Shape outline;
-    
-    
-    public Debris(int x, int y)
-    {
-       int randDirection;
-       int randAmount;
-       
-       randAmount = RANDOM.nextInt();
-       randDirection = RANDOM.nextInt();
 
-       
+    public Debris (double x, double y)
+    {
+
+        
+        setPosition(x, y);
+        setVelocity(RANDOM.nextInt(5), RANDOM.nextDouble() * 2 * Math.PI);
+        setRotation(2 * Math.PI * RANDOM.nextDouble());
+        generateDebrisOutline(x, y);
+        
+
+        
     }
-    
-    public void generate()
+
+    public void generateDebrisOutline (double x, double y)
     {
         Path2D.Double poly = new Path2D.Double();
-        
-      
-        poly.moveTo(0, -30);
-        poly.lineTo(28, -15);
-        poly.lineTo(20, 20);
-        poly.lineTo(4, 8);
-        poly.lineTo(-1, 30);
-        poly.lineTo(-12, 15);
-        poly.lineTo(-5, 2);
-        poly.lineTo(-25, 7);
-        poly.lineTo(-10, -25);
+
+        poly.moveTo(x, y);
+        poly.lineTo(x + RANDOM.nextInt(5), y + RANDOM.nextInt(7));
         poly.closePath();
-        
-        
-        
+
         outline = poly;
     }
-    
-    
+
     @Override
     protected Shape getOutline ()
     {
         return outline;
     }
-    
+
+    @Override
+    public void collidedWith (Participant p)
+    {
+        
+    }
 
 }
