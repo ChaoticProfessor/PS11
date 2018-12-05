8f5634b2605d63b378513bc8c60b6e85d0a3c66b
parker-the-king <45435862+parker-the-king@users.noreply.github.com>
2018-12-05 09:11:23 -0700

    Update Alien.java

--- Alien.java
+++ Alien.java
@@ -93,7 +93,7 @@ public class Alien extends Participant implements ShipDestroyer
             Participant.expire(this);
 
             // Generate Debris
-            controller.placeDebris(this.getX(), this.getY());
+            
 
             // Inform the controller
 
